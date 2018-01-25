8d3626dfbf5770c5fe23cc230811e466c6ce6759
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-01-25 10:51:32 +0100

    remove more meta information

--- fixDockerUrls.py
+++ fixDockerUrls.py
@@ -13,10 +13,13 @@ with open(sys.argv[1]) as f:
     fExport = yaml.load(f)
 
 for i in fExport['items']:
+    if 'status' in i:
+      del i['status']
     if ('annotations' in i['metadata'] and
        'kubectl.kubernetes.io/last-applied-configuration' in i['metadata']['annotations']):
           del i['metadata']['annotations']['kubectl.kubernetes.io/last-applied-configuration']
     if(i['kind'] == 'ImageStream'):
+        del i['metadata']['annotations']['openshift.io/image.dockerRepositoryCheck']
         tags = i['spec']['tags'][0]
         if tags['annotations'] is not None:
             tags['from']['name'] = tags['annotations']['openshift.io/imported-from']
